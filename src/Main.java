14826432d55bbe0f17f58d83248bd83f95f674fb
Eduardo Ganchala <eduardo.ganchala@epn.edu.ec>
2025-04-23 21:01:58 -0500

    Ejercicio 1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // Crear instancia de los jugadores
+        Jugador jugador1 = new Jugador("Cristiano Ronaldo", "DC",40,7);
+        Jugador jugador2 = new Jugador("Sergio Ramos", "DFC",41,8);
+        Jugador jugador3 = new Jugador("Lionel Messi", "DC",37,10);
+        // Mostrar informacion de los jugadores quemados
+        jugador1.presentar();
+        jugador2.presentar();
+        jugador3.presentar();
+        // Jugador de Scanner
+        Jugador jugador4 = Jugador.presentar1();
+    }
+}
